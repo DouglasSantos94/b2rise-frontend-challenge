718fcf27459072cb76db784dc526699e461bc6b0
Douglas Santos <dgstos94@gmail.com>
2023-07-26 18:46:11 -0300

    feat: create filter logic and pass function to button

--- src/app/search-page/search-page.component.ts
+++ src/app/search-page/search-page.component.ts
@@ -1,6 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { RouterModule } from '@angular/router';
+import { Router, RouterModule } from '@angular/router';
+import { ProductService } from '../product.service';
+import { Product } from '../product';
 
 @Component({
   selector: 'app-search-page',
@@ -9,4 +11,27 @@ import { RouterModule } from '@angular/router';
   templateUrl: './search-page.component.html',
   styleUrls: ['./search-page.component.css'],
 })
-export class SearchPageComponent {}
+export class SearchPageComponent {
+  productService: ProductService = inject(ProductService);
+  productList: Product[] = [];
+
+  constructor(private router: Router) {
+    this.productService.getAllProducts().then((productList: Product[]) => {
+      this.productList = productList;
+    });
+  }
+
+  filterResults(text: string) {
+    if (!text) {
+      this.productService.filteredProducts = this.productList;
+    }
+
+    this.productService.filterProducts(
+      this.productList.filter((product: Product) =>
+        product.description.toLowerCase().includes(text.toLowerCase())
+      )
+    );
+
+    this.router.navigate(['/store']);
+  }
+}
