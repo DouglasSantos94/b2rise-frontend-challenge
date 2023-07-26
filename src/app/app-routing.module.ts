55cdbf6078f0c749c32a990bc1a16af0234a9cb4
Douglas Santos <dgstos94@gmail.com>
2023-07-26 16:59:07 -0300

    refactor: change home component name to search-page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-import { HomeComponent } from './home/home.component';
+import { SearchPageComponent } from './search-page/search-page.component';
 import { StoreComponent } from './store/store.component';
 import { CartComponent } from './cart/cart.component';
 
 const routes: Routes = [
-  { path: '', component: HomeComponent },
+  { path: '', component: SearchPageComponent },
   { path: 'store', component: StoreComponent },
   { path: 'cart', component: CartComponent },
 ];
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,11 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HomeComponent } from './home/home.component';
+import { SearchPageComponent } from './search-page/search-page.component';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule, HomeComponent],
+  imports: [BrowserModule, AppRoutingModule, SearchPageComponent],
   providers: [],
   bootstrap: [AppComponent],
 })
