240abe1127d4fa9d6592ddfdf34b1c97a4afa475
Douglas Santos <dgstos94@gmail.com>
2023-07-26 17:10:47 -0300

    feat: add link to cart route

--- src/app/search-page/search-page.component.ts
+++ src/app/search-page/search-page.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-search-page',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, RouterModule],
   templateUrl: './search-page.component.html',
   styleUrls: ['./search-page.component.css'],
 })
