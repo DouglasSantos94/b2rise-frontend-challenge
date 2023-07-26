1a22f3e54f77b708b589462bea4063e62c1a0f4d
Douglas Santos <dgstos94@gmail.com>
2023-07-26 16:18:15 -0300

    feat: add cart route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,10 +3,12 @@ import { RouterModule, Routes } from '@angular/router';
 
 import { HomeComponent } from './home/home.component';
 import { StoreComponent } from './store/store.component';
+import { CartComponent } from './cart/cart.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'store', component: StoreComponent },
+  { path: 'cart', component: CartComponent },
 ];
 
 @NgModule({
