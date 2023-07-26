ee3137dcf0b1a04f076300e0203038ee5d1cf54f
Douglas Santos <dgstos94@gmail.com>
2023-07-26 14:23:52 -0300

    feat: create initial search page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SearchPageComponent } from './search-page/search-page.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent],
+  imports: [BrowserModule, AppRoutingModule, SearchPageComponent],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
