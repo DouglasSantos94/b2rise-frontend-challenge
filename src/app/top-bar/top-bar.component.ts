2bad1d90901ef588cbe5e8317180be44765a011a
Douglas Santos <dgstos94@gmail.com>
2023-07-26 21:44:05 -0300

    feat: add filter on topBar component

--- src/app/top-bar/top-bar.component.ts
+++ src/app/top-bar/top-bar.component.ts
@@ -1,13 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ProductService } from '../product.service';
+import { Product } from '../product';
 
 @Component({
   selector: 'app-top-bar',
   standalone: true,
   imports: [CommonModule],
   templateUrl: './top-bar.component.html',
-  styleUrls: ['./top-bar.component.css']
+  styleUrls: ['./top-bar.component.css'],
 })
 export class TopBarComponent {
+  productService: ProductService = inject(ProductService);
 
+  filterResults(text: string) {
+    if (!text) {
+      this.productService.filteredProducts = this.productService.allProducts;
+    }
+
+    this.productService.filterProducts(
+      this.productService.allProducts.filter((product: Product) =>
+        product.description.toLowerCase().includes(text.toLowerCase())
+      )
+    );
+  }
 }
