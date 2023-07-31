59eed38eef84961e425eb5753d7ab756f0f7c4a2
Douglas Santos <dgstos94@gmail.com>
2023-07-31 12:42:07 -0300

    style: stylize search page input

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 module.exports = {
   content: ["./src/**/*.{html,ts}"],
   theme: {
-    extend: {},
+    extend: {
+      width: {
+        836: "52.25rem",
+      },
+    },
   },
   plugins: [],
 };
